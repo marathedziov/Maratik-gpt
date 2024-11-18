7a212ee80b8ae5a953ff5ec63347088a39bf2644
odziov <mrtdzv@gmail.com>
2024-11-18 12:54:05 +0300

    бот 1.0

--- start.sh
+++ start.sh
@@ -6,20 +6,11 @@ set -eu
 # Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
 export PYTHONUNBUFFERED=true
 
-# Install Python 3 virtual env
-VIRTUALENV=./venv
-
-# Создаем виртуальное окружение, если его нет
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
-
-# Активируем виртуальное окружение
-source $VIRTUALENV/bin/activate
-
-# Устанавливаем pip, если его нет в виртуальном окружении
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/pip/3.7/get-pip.py | python3
+# Устанавливаем pip, если его нет
+if ! command -v pip &> /dev/null
+then
+    echo "pip not found. Installing..."
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/pip/3.7/get-pip.py | python3
 fi
 
 # Устанавливаем зависимости из requirements.txt
